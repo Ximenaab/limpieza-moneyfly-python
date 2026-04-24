3b306b7c1254e054ffa9e24a826b678becdc867f
Ximena Beltrán <amelrosereds@gmail.com>
2026-04-24 10:07:33 -0500

    cambio a decimales cortos en persistencia

--- src/persistencia.py
+++ src/persistencia.py
@@ -1,7 +1,10 @@
 def guardar_datos(df, ruta="data/processed/gastos_limpios.csv"):
-    df.to_csv(ruta, index=False)
+    df.to_csv(ruta, index=False, float_format="%.2f")
     print(f"✅ Archivo guardado en: {ruta}")
 
 def guardar_resumen(df_resumen, ruta="data/processed/resumen_analitico_usuarios.csv"):
-    df_resumen.to_csv(ruta, index=False)
+    # Redondear columnas numéricas a 2 decimales
+    df_resumen["total_gastado"] = df_resumen["total_gastado"].round(2)
+    df_resumen["promedio_gasto"] = df_resumen["promedio_gasto"].round(2)
+    df_resumen.to_csv(ruta, index=False, float_format="%.2f")
     print(f"✅ Resumen guardado en: {ruta}")
